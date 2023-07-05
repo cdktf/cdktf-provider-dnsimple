d34a5b301c555fb2707a8a24937440227b3cb286
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-05 08:46:37 +0000

    chore(deps): Updated

--- src/domain-delegation/index.ts
+++ src/domain-delegation/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/dnsimple/dnsimple/1.1.2/docs/resources/domain_delegation
 // generated from terraform resource schema
 
