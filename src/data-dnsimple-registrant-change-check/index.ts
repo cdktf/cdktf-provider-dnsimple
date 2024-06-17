f66e329a02db3c5b3575a9a705f998e07a0d5872
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-17 13:49:52 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-dnsimple-registrant-change-check/index.ts
+++ src/data-dnsimple-registrant-change-check/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/dnsimple/dnsimple/1.6.0/docs/data-sources/registrant_change_check
 // generated from terraform resource schema
 
